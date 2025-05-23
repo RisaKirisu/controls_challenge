8984b6bc1250fcb23cd99fa3138dfc9e6f707037
Peiyan Wu <wumingze2014@gmail.com>
2025-05-23 00:44:05 -0600

    pw: extract eps

--- PPO_control/PPO_Loss.py
+++ PPO_control/PPO_Loss.py
@@ -1,10 +1,8 @@
-from typing import Tuple
-
 import torch
 
+EPS = 1e-8
 def actor_loss(action_p: torch.Tensor, action_p_old: torch.Tensor, advantage: torch.Tensor, clip_eps: float) -> torch.Tensor:
-    eps = 1e-8
-    r = action_p / (action_p_old + eps)
+    r = action_p / (action_p_old + EPS)
     L_clip = torch.mean(torch.min(r * advantage, torch.clip(r, 1 - clip_eps, 1 + clip_eps) * advantage))
     return -L_clip
 
@@ -17,7 +15,6 @@ def generalized_advantage_estimation(reward: torch.Tensor, value: torch.Tensor, 
     reward: batched rewards for each time step (batch_size, T)
     value: batched value estimation for each time step plus last state (batch_size, T+1)
     '''
-    eps = 1e-8
 
     advantage = torch.zeros_like(reward)
     last_adv = 0
@@ -27,7 +24,7 @@ def generalized_advantage_estimation(reward: torch.Tensor, value: torch.Tensor, 
         last_adv = advantage[:, t]
     
     # Normalize across batch
-    advantage = (advantage - advantage.mean()) / (advantage.std() + eps)
+    advantage = (advantage - advantage.mean()) / (advantage.std() + EPS)
 
     return advantage
 
@@ -36,15 +33,13 @@ def calculate_value_target(reward: torch.Tensor, value: torch.Tensor, discount_f
     reward: batched rewards for each time step (batch_size, T)
     value: batched value estimation for each time step plus last state (batch_size, T+1)
     '''
-    eps = 1e-8
-
     v_target = torch.zeros_like(reward)
     last_reward = value[:, -1]
     for t in reversed(range(reward.shape[1])):
         v_target[:, t] = reward[:, t] + discount_factor * last_reward
         last_reward = v_target[:, t]
 
-    v_target = (v_target - v_target.mean()) / (v_target.std() + eps)
+    v_target = (v_target - v_target.mean()) / (v_target.std() + EPS)
 
     return v_target
 
@@ -54,7 +49,6 @@ def calculate_value_target_vec(reward: torch.Tensor, value: torch.Tensor, discou
     value: batched value estimation for each time step plus last state (batch_size, T+1)
     '''
     T = reward.shape[1]
-    eps = 1e-8
 
     # Calculate discounted sum of rewards
     discount_factors = discount_factor ** torch.arange(T, dtype=torch.float32, device=reward.device)
@@ -64,7 +58,7 @@ def calculate_value_target_vec(reward: torch.Tensor, value: torch.Tensor, discou
     # Add the discounted final value estimate
     v_target = v_target + torch.outer(value[:, -1], (discount_factor * discount_factors).flip(0))
 
-    v_target = (v_target - v_target.mean()) / (v_target.std() + eps)
+    v_target = (v_target - v_target.mean()) / (v_target.std() + EPS)
     
     return v_target
 
@@ -74,7 +68,7 @@ def generalized_advantage_estimation_vec(reward: torch.Tensor, value: torch.Tens
     value: batched value estimation for each time step plus last state (batch_size, T+1)
     '''
     T = reward.shape[1]
-    eps = 1e-8
+
     
     # Calculate delta
     delta = reward + discount_factor * value[:, 1:] - value[:, :-1]
@@ -87,6 +81,6 @@ def generalized_advantage_estimation_vec(reward: torch.Tensor, value: torch.Tens
     advantage = advantage / discount_factors.unsqueeze(0)
 
     # Normalize across batch
-    advantage = (advantage - advantage.mean()) / (advantage.std() + eps)
+    advantage = (advantage - advantage.mean()) / (advantage.std() + EPS)
     
     return advantage
